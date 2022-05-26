b2cdfe43ca2ef57f239bb454b360f64a649586b6
Иванов Евгений <eugenelly@icloud.com>
2022-05-26 23:23:27 +0300

    add module-16

---
+++ web/js/16. Redux/dist/main.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Plugin/Preset files are not allowed to export objects, only functions. In C:\\Users\\Eugene\\Desktop\\learn\\web\\Skillbox\\homeWork\\4. JavaScript с нуля\\15. React\\node_modules\\babel-preset-react\\lib\\index.js\n    at createDescriptor (C:\\Users\\Eugene\\Desktop\\learn\\web\\Skillbox\\homeWork\\4. JavaScript с нуля\\15. React\\node_modules\\@babel\\core\\lib\\config\\config-descriptors.js:211:11)\n    at createDescriptor.next (<anonymous>)\n    at step (C:\\Users\\Eugene\\Desktop\\learn\\web\\Skillbox\\homeWork\\4. JavaScript с нуля\\15. React\\node_modules\\gensync\\index.js:261:32)\n    at C:\\Users\\Eugene\\Desktop\\learn\\web\\Skillbox\\homeWork\\4. JavaScript с нуля\\15. React\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\Users\\Eugene\\Desktop\\learn\\web\\Skillbox\\homeWork\\4. JavaScript с нуля\\15. React\\node_modules\\gensync\\index.js:223:11)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ web/js/16. Redux/src/contants.js
@@ -0,0 +1,3 @@
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const SET_NEW_COMMENT = 'SET_NEW_COMMENT'
